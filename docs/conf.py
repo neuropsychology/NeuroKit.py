bb99f0314f52c62bf7efb8bd2cb2a41131e49d5b
DominiqueMakowski <dom.mak19@gmail.com>
2017-03-31 12:06:02 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,9 +21,9 @@ import os
 import sys
 import mock
 
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('../..'))
 
-MOCK_MODULES = ['numpy', 'scipy', 'pandas', 'mne', 'nolds', 'bioread', "sklearn", "matplotlib", "biosppy"]
+MOCK_MODULES = ['numpy', 'scipy', 'pandas', 'mne', 'nolds', 'bioread', "sklearn", "matplotlib", "biosppy", "hrv"]
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
 
