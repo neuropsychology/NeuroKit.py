a72749e3cc2e956a8b4976741c13392ff77932e9
DominiqueMakowski <dom.mak19@gmail.com>
2017-04-12 23:11:53 +0200

    try

--- examples/UnderDev/bio_rsa_dev.py
+++ examples/UnderDev/bio_rsa_dev.py
@@ -11,7 +11,7 @@ sampling_rate = 1000
 ecg_filtered = dict(biosppy.ecg.ecg(df["ECG"], show=False))["filtered"]
 rpeaks = dict(biosppy.ecg.ecg(df["ECG"], show=False))["rpeaks"]
 rsp_filtered = dict(biosppy.resp.resp(df["RSP"], show=False))["filtered"]
-#resp_cycles = dict(biosppy.resp.resp(df["RSP"]))["zeros"]  # Returns zero crossings rather than beginning and end of cycles
+#resp_cycles = dict(biosppy.resp.resp(df["RSP"]))["zeros"]  # Returns zero crossings rather than beginnings of cycles
 
 
 """
@@ -19,45 +19,14 @@ Biopac's notice:
 The software automatically measures the minimum and maximum R-R intervals during each respiration cycle. Breath-to-breath values are reported for the Cycle number, Time of the cycle, Minimum R-R interval, Maximum R-R interval and the Respiratory Sinus Arrhythmia value (difference between the minimum and maximum R-R intervals) with option to show logarithmic scaling of the result
 
 https://www.biopac.com/?app-advanced-feature=respiratory-sinus-arrhythmia-rsa-analysis
+
+See also this:
+http://ieeexplore.ieee.org.sci-hub.cc/document/470252/?reload=true
 """
 
 # ==============================================================================
 def bio_rsa(ecg_filtered, rsp_filtered, rpeaks, sampling_rate=1000):
-    """
-    Computation of Respiratory Sinus Arrythmia (RSA).
 
-    Parameters
-    ----------
-    ecg_filtered =  array
-        ECG signal array.
-    rsp_filtered =  array
-        Respiratory signal array.
-    sampling_rate = int
-        Sampling rate (samples/second).
-
-
-    Returns
-    ----------
-    rsa = dict
-        Dict containing RSA features.
-
-
-    Example
-    ----------
-    >>> import neurokit as nk
-    >>>
-    >>>
-
-    Authors
-    ----------
-    Dominique Makowski
-
-    Dependencies
-    ----------
-    - numpy
-    - pandas
-
-    """
     rri = np.diff(rpeaks)
 
     return()
