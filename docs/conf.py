ea2ccf2489e64438003dfddf69f54e3bc07f587b
Dominique Makowski <dom.mak19@gmail.com>
2017-04-13 12:00:43 +0200

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,8 @@ MOCK_MODULES = [
         "biosppy",
         "hrv",
         "Pillow",
-        "cvxopt"]
+        "cvxopt",
+        "cvxopt.solvers"]
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
