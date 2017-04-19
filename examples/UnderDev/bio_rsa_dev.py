e60d75ad4815b173d03aada76b38609efcc57863
DominiqueMakowski <dom.mak19@gmail.com>
2017-04-19 08:11:50 +0200

    adding rhenan's changes

--- examples/UnderDev/bio_rsa_dev.py
+++ examples/UnderDev/bio_rsa_dev.py
@@ -1,42 +1,54 @@
 # -*- coding: utf-8 -*-
+import biosppy
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import biosppy
 
-
-
-df = pd.read_csv("data_rsa.csv")
-
-sampling_rate = 1000
-ecg = df["ECG"]
-rsp = df["RSP"]
-
-
+plt.ion()
 
 """
 Biopac's notice:
 The software automatically measures the minimum and maximum R-R intervals during each respiration cycle. Breath-to-breath values are reported for the Cycle number, Time of the cycle, Minimum R-R interval, Maximum R-R interval and the Respiratory Sinus Arrhythmia value (difference between the minimum and maximum R-R intervals) with option to show logarithmic scaling of the result
-
 https://www.biopac.com/?app-advanced-feature=respiratory-sinus-arrhythmia-rsa-analysis
-
 See also this:
 http://ieeexplore.ieee.org.sci-hub.cc/document/470252/?reload=true
 """
 
+
 # ==============================================================================
-def bio_rsa(ecg, rsp, sampling_rate=1000):
+def bio_rsa(rpeaks, rsp_zero_crossings):
+    cycles_rri = []
+    for idx in range(len(rsp_zero_crossings) - 1):
+        cycle_init = rsp_zero_crossings[idx]
+        cycle_end = rsp_zero_crossings[idx + 1]
+        cycles_rri.append(rpeaks[np.logical_and(rpeaks >= cycle_init,
+                                                rpeaks < cycle_end)])
+    return(cycles_rri)
 
-    # Compute useful features
-    ecg_filtered = dict(biosppy.ecg.ecg(df["ECG"], show=False))["filtered"]
-    rpeaks = dict(biosppy.ecg.ecg(df["ECG"], show=False))["rpeaks"]
-    rsp_filtered = dict(biosppy.resp.resp(df["RSP"], show=False))["filtered"]
-    rsp_zero_crossings = dict(biosppy.resp.resp(df["RSP"], show=False))["zeros"]
+
+def _get_useful_signals(data, *cols):
+    ecg = biosppy.ecg.ecg(data[cols[0]], show=False)
+    rsp = biosppy.resp.resp(data[cols[1]], show=False)
+
+    rpeaks = ecg['rpeaks']
+    rsp_zero_crossings = rsp['zeros']
+
+    return(ecg, rsp, rpeaks, rsp_zero_crossings)
 
 
-    # RR interval
-    rri = np.diff(rpeaks)
-
-    return()
 # ==============================================================================
 
+if __name__ == "__main__":
+    data = pd.read_csv('data_rsa.csv')
+    ecg, rsp, rpeaks, rsp_zero_crossings = _get_useful_signals(data, 'ECG',
+                                                               'RSP')
+    cycles_rri = bio_rsa(rpeaks, rsp_zero_crossings)
 
+    # Some plots to check if the RRi found within the resp cycles are correct.
+    # Later use tdd
+
+    plt.figure()
+    plt.plot(ecg['ts'], ecg['filtered'])
+    plt.plot(rsp['ts'], rsp['filtered'], 'k')
+    plt.plot(rsp['ts'][rsp_zero_crossings],
+             rsp['filtered'][rsp_zero_crossings], 'ro')
