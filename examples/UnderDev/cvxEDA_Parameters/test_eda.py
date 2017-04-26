85c907b2b5c46214fd0f6f555dca175b3a433cd9
DominiqueMakowski <dom.mak19@gmail.com>
2017-04-26 11:18:33 +0200

    test

--- examples/UnderDev/cvxEDA_Parameters/test_eda.py
+++ examples/UnderDev/cvxEDA_Parameters/test_eda.py
@@ -1,6 +1,26 @@
 import neurokit as nk
 import pandas as pd
+import matplotlib.pyplot as plt
+import biosppy
+import numpy as np
+eda = pd.Series.from_csv("test_eda.csv")  # 100 Hz
+
+#proc = nk.eda_process(eda, sampling_rate=100, use_cvxEDA=True, cvxEDA_normalize=True, cvxEDA_alpha=0.0008, cvxEDA_gamma=0.01)
+proc = nk.eda_process(eda, sampling_rate=100, use_cvxEDA=False, cvxEDA_normalize=True, cvxEDA_alpha=0.0008, cvxEDA_gamma=0.01)
 
 
+print(len(proc["EDA"]["SCR_Peaks_Indexes"]))
 
-eda = pd.Series.from_csv("test_eda.csv")
+proc["df"].plot()
+for i in proc["EDA"]["SCR_Peaks_Indexes"]:
+    plt.axvline(i, color='red')
+#
+#eda2 = proc["df"]["EDA_Filtered"]
+#scr = dict(biosppy.eda.basic_scr(eda2, sampling_rate=100))
+#print(len(scr["peaks"]))
+#
+#
+#onsets, peaks, amps = biosppy.eda.kbk_scr(eda2, sampling_rate=100)
+#print(len(peaks))
+#onsets, peaks, amps = biosppy.eda.basic_scr(eda2, sampling_rate=100)
+#print(len(peaks))
