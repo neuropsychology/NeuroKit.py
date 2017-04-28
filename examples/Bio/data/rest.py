509c5a8007f7ee8ce4975e2169b295a402e0df1e
Dominique Makowski <dom.mak19@gmail.com>
2017-04-28 18:06:19 +0200

    disabling HRV 2

--- examples/Bio/data/rest.py
+++ examples/Bio/data/rest.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib
 import seaborn as sns
+import biosppy
 
 df = pd.read_csv("https://raw.githubusercontent.com/neuropsychology/NeuroKit.py/master/examples/Bio/data/bio_rest.csv")
 events = nk.find_events(df["Photosensor"], cut="lower")
@@ -11,5 +12,10 @@ df = nk.create_epochs(df, events["onsets"], duration=events["durations"], onset=
 df = df[0]  # Select the first element of that list.
 
 bio = nk.bio_process(ecg=df["ECG"], rsp=df["RSP"], eda=df["EDA"], add=df["Photosensor"])
-
-ecg = nk.ecg_process(ecg=df["ECG"])
+#
+#ecg = nk.ecg_process(ecg=df["ECG"])
+#
+#biosppy_ecg = biosppy.ecg.ecg(df["ECG"], show=False)
+#rri = np.diff(biosppy_ecg["rpeaks"])
+#
+#
