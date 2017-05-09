d6152ad68cee85d072e7597e757ce2dff8f0a3ed
Dominique Makowski <dom.mak19@gmail.com>
2017-05-09 18:52:36 +0200

    complexity hotfix

--- examples/UnderDev/ECG_Entropy/test.py
+++ examples/UnderDev/ECG_Entropy/test.py
@@ -6,67 +6,11 @@ import nolds
 
 
 
-#==============================================================================
-#
-#==============================================================================
-def sample_entropy(time_series, sample_length, tolerance=None):
-    """Calculate and return Sample Entropy of the given time series.
-    Distance between two vectors defined as Euclidean distance and can
-    be changed in future releases
-    Args:
-        time_series: Vector or string of the sample data
-        sample_length: Number of sequential points of the time series
-        tolerance: Tolerance (default = 0.1...0.2 * std(time_series))
-    Returns:
-        Vector containing Sample Entropy (float)
-    References:
-        [1] http://en.wikipedia.org/wiki/Sample_Entropy
-        [2] http://physionet.incor.usp.br/physiotools/sampen/
-        [3] Madalena Costa, Ary Goldberger, CK Peng. Multiscale entropy analysis
-            of biological signals
-    """
-    if tolerance is None:
-        tolerance = 0.1 * np.std(time_series)
-
-    n = len(time_series)
-    prev = np.zeros(n)
-    curr = np.zeros(n)
-    A = np.zeros((sample_length, 1))  # number of matches for m = [1,...,template_length - 1]
-    B = np.zeros((sample_length, 1))  # number of matches for m = [1,...,template_length]
-
-    for i in range(n - 1):
-        nj = n - i - 1
-        ts1 = time_series[i]
-        for jj in range(nj):
-            j = jj + i + 1
-            if abs(time_series[j] - ts1) < tolerance:  # distance between two vectors
-                curr[jj] = prev[jj] + 1
-                temp_ts_length = min(sample_length, curr[jj])
-                for m in range(int(temp_ts_length)):
-                    A[m] += 1
-                    if j < n - 1:
-                        B[m] += 1
-            else:
-                curr[jj] = 0
-        for j in range(nj):
-            prev[j] = curr[j]
-
-    N = n * (n - 1) / 2
-    B = np.vstack(([N], B[:sample_length - 1]))
-    similarity_ratio = A / B
-    se = - np.log(similarity_ratio)
-    se = np.reshape(se, -1)
-    return se
-#==============================================================================
-#
-#==============================================================================
-
-
 df = pd.read_csv('data.csv')
-df = nk.bio_process(ecg=df["ECG"])
+df = nk.ecg_process(ecg=df["ECG"])
 rri = df["ECG"]["RRis"]
 
-nolds.sampen(rri, 2, 0.1*np.std(rri), dist="euler")
-sample_entropy(rri,2, 0.1*np.std(rri))[1]
-
-
+#nolds.sampen(rri, 2, 0.1*np.std(rri), dist="euler")
+#sample_entropy(rri,2, 0.1*np.std(rri))[1]
+#
+#
