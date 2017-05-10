d9480699e6cd5dc26af40fe9bb92e56e443249b2
DominiqueMakowski <dom.mak19@gmail.com>
2017-05-10 10:25:41 +0200

    hotfix

--- neurokit/bio/bio_ecg.py
+++ neurokit/bio/bio_ecg.py
@@ -189,7 +189,7 @@ def ecg_process(ecg, rsp=None, sampling_rate=1000, resampling_method="bfill"):
 
     # Complexity
     processed_ecg["ECG"]["Complexity"] = {}
-    chaos = nk.complexity(rri, lyap_r=False, lyap_e=False)
+    chaos = complexity(rri, lyap_r=False, lyap_e=False)
     chaos = pd.Series(chaos)
     chaos.index = ["ECG_Complexity_" + s for s in chaos.index]
     processed_ecg["ECG"]["Complexity"] = chaos.to_dict()
