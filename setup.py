d9ea0b884d036cae0475b3cbe37dd0cbdae00465
Dominique Makowski <dom.mak19@gmail.com>
2017-07-04 16:24:11 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,6 +43,5 @@ classifiers = [
         'Operating System :: Unix',
         'Operating System :: MacOS',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7']
+        'Programming Language :: Python :: 3.6']
 )
