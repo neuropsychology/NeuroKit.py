eca1a49c4d7e8ff6e6145aeaa9f631db6edc714c
Dominique Makowski <dom.mak19@gmail.com>
2017-07-04 17:00:16 +0200

    HRV hotfix

--- neurokit/bio/bio_ecg.py
+++ neurokit/bio/bio_ecg.py
@@ -570,7 +570,7 @@ def ecg_hrv(rpeaks, sampling_rate=1000):
     beats_times -= beats_times[0]
     beats_times = np.delete(beats_times, artifacts_indices)  # delete also the artifact beat moments
     try:
-        RRi = discrete_to_continuous(RRis, beats_times, 1000)  # Interpolation using 3rd order spline
+        RRi = discrete_to_continuous(RRis, beats_times, sampling_rate)  # Interpolation using 3rd order spline
     except TypeError:
         print("NeuroKit Warning: ecg_hrv(): Sequence too short to compute HRV.")
         return(hrv)
