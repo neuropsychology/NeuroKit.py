07a71fd2d07ca22b263e4b5e4d3122beb04910f7
DominiqueMakowski <dom.mak19@gmail.com>
2017-08-23 17:42:09 +0200

    allow bayesian CI in plot_eeg_erp

--- neurokit/eeg/eeg_erp.py
+++ neurokit/eeg/eeg_erp.py
@@ -65,19 +65,19 @@ def eeg_erp(eeg, windows=None, index=None, include="all", exclude=None, hemisphe
 # ==============================================================================
 # ==============================================================================
 # ==============================================================================
-def plot_eeg_erp(all_epochs, include="all", exclude=None, hemisphere="both", central=True, title=None, colors=None, gfp=False, ci=0.95, invert_y=False, linewidth=1, filter_hfreq=None, ci_apha=0.333):
+def plot_eeg_erp(all_epochs, include="all", exclude=None, hemisphere="both", central=True, title=None, colors=None, gfp=False, ci=0.95, ci_apha=0.333, ci_method="parametric", invert_y=False, linewidth=1, linestyle="-", filter_hfreq=None):
     """
     """
     # Preserve original
-    all_epochs_original = all_epochs.copy()
+    all_epochs_current = all_epochs.copy()
 
     # Filter using Savitzky-Golay polynomial method
     if (filter_hfreq is not None) and (isinstance(filter_hfreq, int)):
-        for participant, epochs in all_epochs_original.items():
-            all_epochs_original[participant] = epochs.copy().savgol_filter(filter_hfreq)
+        for participant, epochs in all_epochs_current.items():
+            all_epochs_current[participant] = epochs.copy().savgol_filter(filter_hfreq)
 
     # Transform to evokeds
-    all_evokeds = eeg_to_all_evokeds(all_epochs_original)
+    all_evokeds = eeg_to_all_evokeds(all_epochs_current)
 
     data = {}
     for participant, epochs in all_evokeds.items():
@@ -87,10 +87,36 @@ def plot_eeg_erp(all_epochs, include="all", exclude=None, hemisphere="both", cen
         for condition, epoch in epochs.items():
             data[condition].append(epoch)
 
+    conditions = list(data.keys())
+
+    # Line styles
+    if isinstance(linestyle, str):
+        linestyle = [linestyle] * len(conditions)
+    elif isinstance(linestyle, list) and len(linestyle) >= len(conditions):
+        pass
+    elif isinstance(linestyle, dict) and len(linestyle.keys()) >= len(conditions):
+        linestyle = [linestyle[cond] for cond in conditions]
+    else:
+        print("NeuroKit Warning: plot_eeg_erp(): linestyle must be either a str, a list or a dict.")
+
+
+    # Colors
+    if isinstance(colors, str):
+        colors = {condition: colors for condition in conditions}
+    elif isinstance(colors, list) and len(colors) >= len(conditions):
+        colors=  {condition: colors[index] for index, condition in enumerate(conditions)}
+    elif isinstance(colors, dict) and len(colors.keys()) >= len(conditions):
+        pass
+    elif colors is None:
+        pass
+    else:
+        print("NeuroKit Warning: plot_eeg_erp(): colors must be either a str, a list, a dict or None.")
+
+
     # Modify styles
     styles = {}
-    for condition in data.keys():
-        styles[condition] = {"linewidth": linewidth}
+    for index, condition in enumerate(conditions):
+        styles[condition] = {"linewidth": linewidth, "linestyle": linestyle[index]}
 
 
     # Select electrodes
@@ -98,9 +124,9 @@ def plot_eeg_erp(all_epochs, include="all", exclude=None, hemisphere="both", cen
 
     # Plot
     try:
-        plot = mne.viz.plot_compare_evokeds(data, picks=picks, colors=colors, styles=styles, title=title, gfp=gfp, ci=ci, invert_y=invert_y, ci_alpha=ci_apha)
+        plot = mne.viz.plot_compare_evokeds(data, picks=picks, colors=colors, styles=styles, title=title, gfp=gfp, ci=ci, invert_y=invert_y, ci_alpha=ci_apha, ci_method=ci_method)
     except TypeError:
-        print("NeuroKit Warning: plot_eeg_erp(): You're using a version of mne that does not support ci_alpha parameter. Leaving defaults.")
+        print("NeuroKit Warning: plot_eeg_erp(): You're using a version of mne that does not support ci_alpha or ci_method parameters. Leaving defaults.")
         plot = mne.viz.plot_compare_evokeds(data, picks=picks, colors=colors, styles=styles, title=title, gfp=gfp, ci=ci, invert_y=invert_y)
 
     return(plot)
