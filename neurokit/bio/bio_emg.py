b2c5d0c9812b1ec640fe24e486a9d87b1596a8a7
DominiqueMakowski <dom.mak19@gmail.com>
2017-08-25 16:46:10 +0200

    EMG processing new features

--- neurokit/bio/bio_emg.py
+++ neurokit/bio/bio_emg.py
@@ -2,10 +2,9 @@
 import pandas as pd
 import numpy as np
 import biosppy
+import scipy
 
 
-
-
 # ==============================================================================
 # ==============================================================================
 # ==============================================================================
@@ -14,7 +13,7 @@ import biosppy
 # ==============================================================================
 # ==============================================================================
 # ==============================================================================
-def emg_process(emg, sampling_rate=1000, emg_names=None):
+def emg_process(emg, sampling_rate=1000, emg_names=None, envelope_freqs=[10, 400], envelope_lfreq=4, activation_treshold="default", activation_n_above=0.25, activation_n_below=1):
     """
     Automated processing of EMG signal.
 
@@ -26,6 +25,17 @@ def emg_process(emg, sampling_rate=1000, emg_names=None):
         Sampling rate (samples/second).
     emg_names : list
         List of EMG channel names.
+    envelope_freqs : list [fc_h, fc_l], optional
+            cutoff frequencies for the band-pass filter (in Hz).
+    envelope_lfreq : number, optional
+            cutoff frequency for the low-pass filter (in Hz).
+    activation_treshold : float
+        minimum amplitude of `x` to detect.
+    activation_n_above : float
+        minimum continuous time (in s) greater than or equal to `threshold` to detect (but see the parameter `n_below`).
+    activation_n_below : float
+        minimum time (in s) below `threshold` that will be ignored in the detection of `x` >= `threshold`.
+
 
     Returns
     ----------
@@ -95,10 +105,196 @@ def emg_process(emg, sampling_rate=1000, emg_names=None):
             pulse_onsets[biosppy_emg['onsets']] = 1
         processed_emg["df"][emg_names[index] + "_Pulse_Onsets"] = pulse_onsets
 
-
         processed_emg["df"][emg_names[index] + "_Filtered"] = biosppy_emg["filtered"]
         processed_emg[emg_names[index]] = {}
         processed_emg[emg_names[index]]["EMG_Pulse_Onsets"] = biosppy_emg['onsets']
 
+        # Envelope
+        envelope = emg_linear_envelope(biosppy_emg["filtered"], sampling_rate=sampling_rate, freqs=envelope_freqs, lfreq=envelope_lfreq)
+        processed_emg["df"][emg_names[index] + "_Envelope"] = envelope
+
+        # Activation
+        if activation_treshold == "default":
+            activation_treshold = 1*np.std(envelope)
+        processed_emg["df"][emg_names[index] + "_Activation"] = emg_find_activation(envelope, sampling_rate=sampling_rate, threshold=1*np.std(envelope), n_above=activation_n_above, n_below=activation_n_below)
 
     return(processed_emg)
+
+
+
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+def emg_tkeo(emg):
+    """
+    Calculates the Teager–Kaiser Energy operator.
+
+    Parameters
+    ----------
+    emg : array
+        raw EMG signal.
+
+    Returns
+    -------
+    tkeo : 1D array_like
+        signal processed by the Teager–Kaiser Energy operator.
+
+    Notes
+    -----
+
+    *Authors*
+
+    - Marcos Duarte
+
+
+
+    *See Also*
+
+    See this notebook [1]_.
+
+    References
+    ----------
+    .. [1] https://github.com/demotu/BMC/blob/master/notebooks/Electromyography.ipynb
+
+    """
+    emg = np.asarray(emg)
+    tkeo = np.copy(emg)
+    # Teager–Kaiser Energy operator
+    tkeo[1:-1] = emg[1:-1]*emg[1:-1] - emg[:-2]*emg[2:]
+    # correct the data in the extremities
+    tkeo[0], tkeo[-1] = tkeo[1], tkeo[-2]
+
+    return(tkeo)
+
+
+
+
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+def emg_linear_envelope(emg, sampling_rate=1000, freqs=[10, 400], lfreq=4):
+    r"""Calculate the linear envelope of a signal.
+
+    Parameters
+    ----------
+    emg : array
+        raw EMG signal.
+    sampling_rate : int
+        Sampling rate (samples/second).
+    freqs : list [fc_h, fc_l], optional
+            cutoff frequencies for the band-pass filter (in Hz).
+    lfreq : number, optional
+            cutoff frequency for the low-pass filter (in Hz).
+
+    Returns
+    -------
+    envelope : array
+        linear envelope of the signal.
+
+    Notes
+    -----
+
+    *Authors*
+
+    - Marcos Duarte
+
+
+
+    *See Also*
+
+    See this notebook [1]_.
+
+    References
+    ----------
+    .. [1] https://github.com/demotu/BMC/blob/master/notebooks/Electromyography.ipynb
+    """
+    emg = emg_tkeo(emg)
+
+    if np.size(freqs) == 2:
+        # band-pass filter
+        b, a = scipy.signal.butter(2, np.array(freqs)/(sampling_rate/2.), btype = 'bandpass')
+        emg = scipy.signal.filtfilt(b, a, emg)
+    if np.size(lfreq) == 1:
+        # full-wave rectification
+        envelope = abs(emg)
+        # low-pass Butterworth filter
+        b, a = scipy.signal.butter(2, np.array(lfreq)/(sampling_rate/2.), btype = 'low')
+        envelope = scipy.signal.filtfilt(b, a, envelope)
+
+    return (envelope)
+
+
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+# ==============================================================================
+def emg_find_activation(envelope, sampling_rate=1000, threshold=0, n_above=0.25, n_below=1):
+    """Detects onset in data based on amplitude threshold.
+
+    Parameters
+    ----------
+    envelope : array
+        Linear envelope of EMG signal.
+    sampling_rate : int
+        Sampling rate (samples/second).
+    threshold : float
+        minimum amplitude of `x` to detect.
+    n_above : float
+        minimum continuous time (in s) greater than or equal to `threshold` to detect (but see the parameter `n_below`).
+    n_below : float
+        minimum time (in s) below `threshold` that will be ignored in the detection of `x` >= `threshold`.
+
+    Returns
+    -------
+    activation : array
+        With 1 when muscle activated and 0 when not.
+
+    Notes
+    -----
+    You might have to tune the parameters according to the signal-to-noise
+    characteristic of the data.
+
+    See this IPython Notebook [1]_.
+
+    References
+    ----------
+    .. [1] http://nbviewer.ipython.org/github/demotu/BMC/blob/master/notebooks/DetectOnset.ipynb
+    """
+    n_above = n_above*sampling_rate
+    n_below = n_below*sampling_rate
+
+
+    envelope = np.atleast_1d(envelope).astype('float64')
+    # deal with NaN's (by definition, NaN's are not greater than threshold)
+    envelope[np.isnan(envelope)] = -np.inf
+    # indices of data greater than or equal to threshold
+    inds = np.nonzero(envelope >= threshold)[0]
+    if inds.size:
+        # initial and final indexes of continuous data
+        inds = np.vstack((inds[np.diff(np.hstack((-np.inf, inds))) > n_below+1], \
+                          inds[np.diff(np.hstack((inds, np.inf))) > n_below+1])).T
+        # indexes of continuous data longer than or equal to n_above
+        inds = inds[inds[:, 1]-inds[:, 0] >= n_above-1, :]
+    if not inds.size:
+        inds = np.array([])  # standardize inds shape
+
+    inds = np.array(inds)
+
+    activation = np.array([0]*len(envelope))
+    for i in inds:
+        activation[i[0]:i[1]] = 1
+    return (activation)
