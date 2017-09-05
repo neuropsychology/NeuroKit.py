c8b98305fb11084d0d097d3d89b5ec6002cb6512
DominiqueMakowski <dom.mak19@gmail.com>
2017-09-05 13:35:27 +0200

    test

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,4 +1,4 @@
-import nose
+import pytest
 import doctest
 import numpy as np
 import pandas as pd
@@ -11,33 +11,33 @@ import matplotlib
 # STATISTICS
 #==============================================================================
 def test_mad():
-    nose.tools.eq_(nk.mad([1, 2, 3, 4, 5, 6, 7]), 2.0)
+    assert nk.mad([1, 2, 3, 4, 5, 6, 7]) == 2.0
 
 def test_z_score():
     raw_scores = [1, 1, 5, 2, 1]
     z = nk.z_score(raw_scores)
-    nose.tools.eq_(int(z.values[3]), 0)
+    assert int(z.values[3]) == 0
 
 def test_find_outliers():
     outliers = nk.find_outliers([1, 2, 1, 5, 666, 4, 1 ,3, 5])
-    nose.tools.eq_(outliers[4], True)
+    assert outliers[4] == True
 
 def test_normal_range():
     bottom, top = nk.normal_range(mean=100, sd=15, treshold=2)
-    nose.tools.eq_(bottom, 70)
+    assert bottom == 70
 
 #==============================================================================
 # ROUTINES
 #==============================================================================
 def test_compute_dprime():
     parameters = nk.compute_dprime(n_Hit=7, n_Miss=4, n_FA=6, n_CR=6)
-    nose.tools.eq_(np.round(parameters["bppd"], 2), -0.5)
+    assert np.round(parameters["bppd"], 2) == -0.5
 
 def test_compute_BMI():
-    nose.tools.eq_(round(nk.compute_BMI(182, 70, 27, "m")['BMI_old'], 2), 21.13)
+    assert round(nk.compute_BMI(182, 70, 27, "m")['BMI_old'], 2) == 21.13
 
 def test_compute_interoceptive_accuracy():
-    nose.tools.eq_(nk.compute_interoceptive_accuracy(5, 3), 0.5)
+    assert nk.compute_interoceptive_accuracy(5, 3) == 0.5
 
 #==============================================================================
 # Plots
@@ -49,7 +49,7 @@ def test_compute_interoceptive_accuracy():
 
 
 if __name__ == '__main__':
-    nose.run(defaultTest=__name__)
+    pytest.main()
     doctest.testmod()
 
 
