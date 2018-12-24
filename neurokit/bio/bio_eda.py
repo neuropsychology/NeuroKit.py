99371029ce839669f3aec0a61e2cdf75d1ced199
ucohen <ucohen1@gmail.com>
2018-12-24 10:19:10 +0200

    python2 compatibility: make bio_eda compatible

--- neurokit/bio/bio_eda.py
+++ neurokit/bio/bio_eda.py
@@ -233,7 +233,7 @@ def cvxEDA(eda, sampling_rate=1000, tau0=2., tau1=0.7, delta_knot=10., alpha=8e-
     - Greco, A., Valenza, G., & Scilingo, E. P. (2016). Evaluation of CDA and CvxEDA Models. In Advances in Electrodermal Activity Processing with Applications for Mental Health (pp. 35-43). Springer International Publishing.
     - Greco, A., Valenza, G., Lanata, A., Scilingo, E. P., & Citi, L. (2016). cvxEDA: A convex optimization approach to electrodermal activity processing. IEEE Transactions on Biomedical Engineering, 63(4), 797-804.
     """
-    frequency = 1/sampling_rate
+    frequency = 1./sampling_rate
 
     # Normalizing signal
     eda = z_score(eda)
